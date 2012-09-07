ee9bf92124fe84402e4776d6a2dd40d0b0fd9608
Ramiro Gomez <web@ramiro.org>
2012-09-07 23:00:19 +0200

    new deployment

--- js/programming-languages-influence-network/graph.js
+++ js/programming-languages-influence-network/graph.js
@@ -1,4 +1,5 @@
 var Graph = {
+  freebaseapi: 'https://usercontent.googleapis.com/freebase/v1/',
   defaultColor: '#eee',
   srcColor: '#67A9CF',
   dstColor: '#EF8A62',
@@ -130,22 +131,7 @@ var Graph = {
         }
       });
     });
-    Graph.events();
     Graph.sig.draw();
-  },
-
-  events: function() {
-    Graph.sig.bind('overnodes',function(event){
-      var hlang = Graph.sig.getNodes(event.content)[0];
-      if (0 == hlang.degree) return;
-      Graph.hlLang(hlang);
-    }).bind('outnodes',function(event){
-      if (Graph.pid && Graph.pid != Graph.defaultPid) {
-        Graph.hlParadigm(Graph.pid);
-      } else {
-        Graph.reset();
-      }
-    });
   }
 
 };
--- js/programming-languages-influence-network/script.js
+++ js/programming-languages-influence-network/script.js
@@ -10,27 +10,56 @@ Language highlighting via search field with autocomplete?
 Cache http request data in sessionStorage
 */
 $(function(){
-  var pmenu = $('#paradigms');
+  var paradigmmenu = $('#paradigms');
+
   Graph.init('sig');
   $('.showhelp').click(function(e){
     e.preventDefault();
     $('#help').modal();
   });
-  pmenu.click(function(e){
+
+  $.getJSON('/js/programming-languages-influence-network/data.json', function(data) {
+    paradigmmenu.append('<li class="active"><a href="' + Graph.defaultPid + '">All languages (' + data.langs.length + ')</a></li>');
+    $.each(data.paradigms, function(idx, item) {
+      paradigmmenu.append('<li><a href="' + item.id + '">' + item.name + ' (' + item.count + ')</a></li>');
+    });
+    Graph.graph(data.langs);
+  });
+
+  paradigmmenu.click(function(e){
     e.preventDefault();
     if ('a' == e.target.nodeName.toLowerCase()) {
       var t = $(e.target);
       var pid = t.attr('href');
-      pmenu.find('li').removeClass('active');
+      paradigmmenu.find('li').removeClass('active');
       t.parent('li').addClass('active');
       Graph.hlParadigm(pid);
     }
   });
-  $.getJSON('/js/programming-languages-influence-network/data.json', function(data) {
-    pmenu.append('<li class="active"><a href="' + Graph.defaultPid + '">All languages (' + data.langs.length + ')</a></li>');
-    $.each(data.paradigms, function(idx, item) {
-      pmenu.append('<li><a href="' + item.id + '">' + item.name + ' (' + item.count + ')</a></li>');
+
+  Graph.sig.bind('upnodes', function(event){
+    var hlang = Graph.sig.getNodes(event.content)[0];
+    var texturl = Graph.freebaseapi + 'text' + hlang.id;
+    $.getJSON(texturl, function(data){
+      var sl = $('#showlang');
+      sl.find('h3').text(hlang.label);
+      var influenced = [];
+      $.each(hlang.attr.influenced, function(idx, item){
+        influenced.push(item.name);
+      });
+      sl.find('.modal-body').html('<div>' + data.result + '... <a href="http://www.freebase.com/view/' + hlang.id + '">view on Freebase</a><h4>Languages Influenced</h4><p>' + influenced.join(', ') + '</p><hr><p>Search for ' + hlang.label + ' books on <a href="http://www.amazon.com/gp/search?ie=UTF8&camp=1789&creative=9325&index=books&keywords=' + encodeURIComponent(hlang.label) + '&linkCode=ur2&tag=xpdt-20">Amazon.com</a></p></div>');
+      sl.modal();
     });
-    Graph.graph(data.langs);
+  }).bind('overnodes',function(event){
+    var hlang = Graph.sig.getNodes(event.content)[0];
+    if (0 == hlang.degree) return;
+    Graph.hlLang(hlang);
+  }).bind('outnodes',function(event){
+    if (Graph.pid && Graph.pid != Graph.defaultPid) {
+      Graph.hlParadigm(Graph.pid);
+    } else {
+      Graph.reset();
+    }
   });
+
 });
