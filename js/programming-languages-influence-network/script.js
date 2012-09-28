392d32d7f2bd840a706d550b29aa4b68a195fa25
Ramiro Gomez <web@ramiro.org>
2012-09-28 11:59:41 +0200

    new deployment

--- js/programming-languages-influence-network/script.js
+++ js/programming-languages-influence-network/script.js
@@ -7,7 +7,12 @@ var langinfo = function(data) {
   $.each(hlang.attr.influenced, function(idx, item){
     influenced.push(item.name);
   });
-  sl.find('.modal-body').html('<div>' + data.result + '... <a href="http://www.freebase.com/view/' + hlang.id + '">view on Freebase</a><h4>Languages Influenced</h4><p>' + influenced.join(', ') + '</p><hr><p>Search for ' + hlang.label + ' books on <a href="http://www.amazon.com/gp/search?ie=UTF8&camp=1789&creative=9325&index=books&keywords=' + encodeURIComponent(hlang.label) + '&linkCode=ur2&tag=xpdt-20">Amazon.com</a></p></div>');
+  var desc = data.result + '... <a href="http://www.freebase.com/view/' + hlang.id + '">view on Freebase</a>';
+  // in case of Ruby include Matz tweet
+  if ('Ruby' === hlang.label) {
+    desc = '<blockquote class="twitter-tweet"><p>NowBrowsing: Programming Languages Influence Network <a href="http://t.co/kzdSlrpt" title="http://exploringdata.github.com/vis/programming-languages-influence-network/">exploringdata.github.com/vis/programmin…</a></p>&mdash; Yukihiro Matsumoto (@yukihiro_matz) <a href="https://twitter.com/yukihiro_matz/status/251612155470823425" data-datetime="2012-09-28T09:19:47+00:00">September 28, 2012</a></blockquote><script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>' + desc;
+  }
+  sl.find('.modal-body').html('<div>' + desc + '<h4>Languages Influenced</h4><p>' + influenced.join(', ') + '</p><hr><p>Search for ' + hlang.label + ' books on <a href="http://www.amazon.com/gp/search?ie=UTF8&camp=1789&creative=9325&index=books&keywords=' + encodeURIComponent(hlang.label) + '&linkCode=ur2&tag=xpdt-20">Amazon.com</a></p></div>');
   sl.modal();
 };
 
