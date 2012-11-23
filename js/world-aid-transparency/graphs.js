b49909f93ec43c602bb0cd206e2fbbc88cfb29bc
Ramiro Gomez <web@ramiro.org>
2012-11-23 13:48:37 +0100

    new deployment

---
+++ js/world-aid-transparency/graphs.js
@@ -0,0 +1,246 @@
+// adapt containerwidth to screen size
+var containerwidth = function(selector) {
+  return parseInt(d3.select(selector).style('width'))
+};
+
+var mapselect = '#map',
+  width = containerwidth(mapselect),
+  height = width * .6,
+  legendh = 30,
+  proj = d3.geo.equirectangular().scale(1).translate([0, 0]),
+  arc = d3.geo.greatArc().precision(1),
+  format = d3.format(',r'),
+  formatpercentage = d3.format(".1%"),
+  projection = d3.geo.mercator().scale(width).translate([0, 0]),
+  path = d3.geo.path().projection(projection);
+
+var tf = function() {
+  return 'translate(' + width / 2 + ',' + height / 1.8 + ')'
+};
+
+var rescale = function() {
+  svg.attr('transform', 'translate(' + d3.event.translate + ')'
+    + ' scale(' + d3.event.scale + ')')
+}
+
+/************** SVG stuff **************/
+var svg = d3.select(mapselect).append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .call(d3.behavior.zoom().on('zoom', function() {rescale()}))
+    .append('svg:g');
+
+svg.append('rect')
+  .attr('class', 'background')
+  .attr('width', width)
+  .attr('height', height);
+
+var gcountries = svg.append('g')
+  .attr('transform', tf)
+  .append('g')
+    .attr('id', 'countries');
+
+var garcs = svg.append('g')
+  .attr('transform', tf)
+  .attr('id', 'arcs');
+
+var drawmap = function(geocountries, colorize, showLinks) {
+  gcountries.selectAll('path').remove();
+  gcountries.selectAll('path')
+    .data(geocountries)
+    .enter().append('path')
+      .attr('d', path)
+      .attr('class', colorize ? colorize : null)
+      .on('click', showCountry)
+      .on('mouseover', function(d) {
+        var t = d3.select(this);
+        t.style('origfill', document.defaultView.getComputedStyle(this,null)['fill']);
+        t.style('fill', '#d0ffd0');
+        showLinks(d.id)
+      })
+      .on('mouseout', function(d) {
+        var t = d3.select(this);
+        t.style('fill', t.style('origfill'));
+      })
+      .append('title')
+        .text(function(d) {return d.properties.name});
+
+  // only remove links when map area is left to avoid link removal when mouse is
+  // over a link, which had an annoying flickering effect
+  // FIXME doesn't work perfect yet, maybe use mousemove and position instead
+  d3.select(mapselect).on('mouseout', function() {
+    var tid = window.event.relatedTarget.id;
+    var tname = window.event.toElement.nodeName.toLowerCase();
+    if ((tid && 'map' == tid) || (tname && 'div' == tname))
+      garcs.selectAll('path.link').remove()
+    })
+};
+
+var drawlinks = function(links, maxamount) {
+  garcs.selectAll('path.link').remove();
+  garcs.selectAll('path.link')
+    .data(links)
+    .enter().append('path')
+      .attr('class', 'link')
+      .style('stroke-width', function(d) {return scaleLink(d, maxamount)})
+      .attr('d', function(d) {
+        var p = {source:countryinfo[d.source].coords, target:countryinfo[d.target].coords};
+        return path(arc(p))
+      });
+}
+
+var drawlegend = function() {
+  var linew = 25,
+    lineoff = 85,
+    yoff = -4;
+
+  // add a rect to cover lines at the bottom of the map to not disturb legend
+  var grect = svg.append('rect')
+    .attr('class', 'bglegend')
+    .attr('y', height - legendh - 15)
+    .attr('width', width)
+    .attr('height', legendh + 15)
+
+  var glegend = svg.append('g')
+    .attr('transform', 'translate(0,' + (height - legendh) +')')
+    .attr('id', 'legend');
+
+  glegend.append('svg:text')
+    .text('Most received')
+
+  glegend.append('svg:text')
+    .text('Most donated')
+    .attr('x', linew * 10 + lineoff + 10)
+
+  // FIXME make this more flexible
+  glegend.selectAll('line.country')
+    .data([0,1,2,3,4,6,7,8,9,10]).enter()
+    .append('svg:line')
+      .attr('class', function(d){return 'q' + d  + '-11'})
+      .attr('x1', function(d, i) {return lineoff + linew * i})
+      .attr('y1', yoff)
+      .attr('x2', function(d, i) {return lineoff + linew + linew * i})
+      .attr('y2', yoff)
+      .attr('stroke-width', 4);
+
+  var ltext = glegend.append('svg:text')
+    .attr('class', 'footer')
+    .attr('dy', 16);
+  ltext.append('svg:tspan')
+    .text('Mouse over a country to show aid flow, click for details, zoom with the mouse wheel, drag with the mouse button pressed.')
+    .attr('x', 0)
+}
+
+// render bar chart
+var bar = function(selector, data) {
+  var maxval = d3.max(data, function(d) { return d.val });
+  var loff = 110,
+    barw = containerwidth(selector) - loff,
+    y = 17,
+    barh = y * data.length,
+    xoff = 10,
+    yoff = 16,
+    wscale = d3.scale.linear().domain([0, maxval]).range(['0px', barw + 'px']),
+    hscale = d3.scale.ordinal().domain(data).rangeBands([0, barh]);
+
+  var bar = d3.select(selector);
+  bar.selectAll('svg').remove();
+  var svg = bar.append('svg')
+    .attr('class', 'bar')
+    .attr('width', barw + loff)
+    .attr('height', barh + yoff);
+
+  // add lablels
+  var labels = svg.selectAll('text')
+    .data(data).enter()
+  // add left label
+  labels.append('text')
+    .attr('x', 0)
+    .attr('y', function(d, i) {return i * y - 4 + yoff})
+    .attr('class', 'info')
+    .text(function(d) {return d.label})
+    .append('title')
+      .text(function(d) {return d.title});
+  // add right label
+  labels.append('text')
+    .attr('x', barw + 9 * xoff)
+    .attr('y', function(d, i) {return i * y - 4 + yoff})
+    .attr('text-anchor', 'end')
+    .text(function(d) {return d.formatval});
+
+  // add actual bars
+  svg.selectAll('rect.fillblue')
+    .data(data)
+    .enter().append('rect')
+      .attr('class', 'fillblue')
+      .attr('x', 3 * xoff)
+      .attr('y', function(d, i) {return i * y})
+      .attr('width', function(d) {return isNaN(d.val) ? 0 : wscale(d.val)})
+      .attr('height', y - 1);
+};
+
+/*
+ * render scatterplot
+ * data struct [{x:1,y:3,label:'label',title:'title'}]
+ */
+var scatterplot = function(selector, data) {
+  var plotw = containerwidth(selector) - 10,
+    ploth = plotw / 2.12,
+    r = 11,
+    padding = 20,
+    paddingl = 30,
+    xmax = d3.max(data, function(d) { return d.x }),
+    ymax = d3.max(data, function(d) { return d.y }),
+    ymin = d3.min(data, function(d) { return d.y }),
+    xscale = d3.scale.linear().nice()
+      .domain([0, xmax]).range([paddingl, plotw - padding]),
+    // map ymax to 0 so small values are below large ones
+    yscale = d3.scale.linear().nice()
+      .domain([ymin, ymax]).range([ploth - padding, padding]),
+    rscale = d3.scale.linear().nice()
+      .domain([0, xmax]).range([r, r]),
+    xaxis = d3.svg.axis().scale(xscale).ticks(5).tickFormat(formatDollar),
+    yaxis = d3.svg.axis().scale(yscale).orient('left').ticks(5).tickFormat(format);
+
+  var title = function(d) {
+    return d.title + ': ' + formatDollar(d.x) + ', ' + format(d.y)
+  }
+
+  var plot = d3.select(selector);
+  plot.selectAll('svg').remove();
+  var svg = plot.append('svg')
+    .attr('class', 'scatterplot')
+    .attr('width', plotw)
+    .attr('height', ploth);
+
+  svg.selectAll('circle.fillblue')
+    .data(data).enter()
+    .append('circle')
+      .attr('class', 'fillblue')
+      .attr('cx', function(d) {return xscale(d.x)})
+      .attr('cy', function(d) {return yscale(d.y)})
+      .attr('r',  function(d) {return rscale(d.x)})
+      .append('title')
+        .text(title);
+
+  svg.selectAll('text')
+    .data(data).enter()
+    .append('text')
+      .attr('class', 'scatterplot')
+      .attr('text-anchor', 'middle')
+      .attr('x', function(d) {return xscale(d.x)})
+      .attr('y', function(d) {return yscale(d.y) + 3})
+      .text(function(d) {return d.label})
+      .append('title')
+        .text(title);
+
+  svg.append('g')
+    .attr('transform', 'translate(0, ' + (ploth - padding) + ')')
+    .attr('class', 'axis')
+    .call(xaxis);
+
+  svg.append('g')
+    .attr('transform', 'translate(' + paddingl + ', 0)')
+    .attr('class', 'axis')
+    .call(yaxis);
+};
---
+++ js/world-aid-transparency/script.js
@@ -0,0 +1,300 @@
+// year with currently most comprehensive dataset, no newer donations data yet
+var year = 2010,
+  defaultlimit = 20,
+  doLimit = true,
+  scatterLimit = 50,
+  format = d3.format(',r'),
+  relation = 'norelate',
+  iselect,
+  donors,
+  reldonors,
+  recipients,
+  relrecipients,
+  scatterrelation,
+  maxreceived,
+  maxdonated,
+  geocountries;
+
+var getSortedCountryLinks = function(cid) {
+  return donations[year].filter(function(d){
+    return ('undefined' !== typeof countryinfo[d.source]
+      && 'undefined' !== typeof countryinfo[d.target]
+      && (cid == d.source || cid == d.target)) ? true : false
+  }).sort(function(a, b) {return b.usd - a.usd})
+};
+
+var showLinks = function(cid) {
+  var l = getSortedCountryLinks(cid);
+  if (l.length) {
+    maxamount = l[0].usd;
+    drawlinks(l, maxamount)
+  }
+};
+
+// scale link using max aid amount given or received
+var scaleLink = function(link, maxamount) {
+  var lscale = d3.scale.linear().domain([0, maxamount]).range([.1, 5]);
+  return lscale(link.usd);
+};
+
+// show aid data for country
+var showCountry = function(d) {
+  var modal = $('#country-aid')
+  var clinks = getSortedCountryLinks(d.id);
+  // determine if country is donor or recipient by larges amount source
+  var type = (clinks[0].source == d.id) ? 'donor' : 'recipient';
+  var heading = 'Aid given: ' + d.properties.name;
+  if ('recipient' == type) {
+    var heading = 'Aid received: ' + d.properties.name;
+  }
+  modal.find('h3').text(heading)
+  var ranking = [];
+  $.each(clinks, function(idx, item) {
+    var label = item.target;
+    if ('recipient' == type) {
+      var label = item.source
+    }
+    ranking.push({
+      label: label,
+      val: item.usd,
+      formatval: formatDollar(item.usd),
+      title: countryinfo[label].name + ' (' + label + '): ' + format(item.usd),
+    })
+  });
+  bar('#country-aid-chart', ranking);
+  modal.modal('show');
+}
+
+// format us dollar values
+var formatDollar = function(val) {
+  var scale = 1000000;
+  if(val < 1 /100) {
+    val = d3.round(val, 3);
+  } else {
+    val = d3.round(val, 2);
+  }
+  if (val > scale) {
+    val = d3.round(val / scale, 0)
+    val += 'M';
+  }
+  return val;
+};
+
+// data structure for scatterplot
+var spdata = function(xdata, key) {
+  var data = [];
+  for (i in xdata) {
+    var country = xdata[i];
+    var x = country.val;
+    var iso = country.label;
+    if (countrystats[year].hasOwnProperty(iso) && countrystats[year][iso].hasOwnProperty(key)) {
+      data.push({
+        'title': countryinfo[iso].name,
+        'label': iso,
+        'x': x,
+        'y': countrystats[year][iso][key]
+      });
+      if ((doLimit && (data.length == defaultlimit)) || data.length == scatterLimit) break;
+    }
+  }
+  return data
+};
+
+// returns a function that is takes relation into account
+var getQuantize = function() {
+  return function(d) {
+    var val;
+    var rel = 1;
+    var maxval = 10; //RdBu 0 = red, 10 = blue 
+    if ('undefined' !== typeof countrystats[year][d.id]) {
+      if (relation && 'norelate' != relation) {
+        // if relation can't be calculated return null to not colorite country
+        if (!countrystats[year][d.id].hasOwnProperty(relation)) return null;
+        rel = countrystats[year][d.id][relation]
+      }
+      if ('undefined' !== typeof countrystats[year][d.id]['received']) {
+        // scale min between 0 (max received) and 4 (min received)
+        val = 4 - 4 * countrystats[year][d.id]['received'] / rel / maxreceived
+      }
+      else if ('undefined' !== typeof countrystats[year][d.id]['donated']) {
+        // scale max between 6 (min donated) and 10 (max donated)
+        val = 6 + 4 * countrystats[year][d.id]['donated'] / rel / maxdonated
+      }
+    }
+    if (null == val) return null;
+    return 'q' + parseInt(~~val) + '-11';
+  }
+};
+
+// for aid rankings
+var aidRanking = function(items, infix) {
+  var ranking = [];
+  $.each(items, function(idx, item){
+    var formatval = formatDollar(item.val);
+    ranking.push({
+      label: item.label,
+      val: item.val,
+      title: countryinfo[item.label].name + ' (' + item.label + ') - ' + infix + format(item.val),
+      formatval: formatval
+    })
+  });
+  return ranking;
+};
+
+// for indicator rankings
+var indicatorRanking = function(items, indicator) {
+  var ranking = [], val, formatval;
+  $.each(items, function(idx, item) {
+    if ('undefined' !== typeof countrystats[year][item.label][indicator]) {
+      val = countrystats[year][item.label][indicator];
+      formatval = format(val);
+    } else {
+      val = 0;
+      formatval = 'NA';
+    }
+    ranking.push({
+      label: item.label,
+      val: val,
+      title: countryinfo[item.label].name + ' (' + item.label + '): ' + formatval,
+      formatval: formatval
+    })
+  });
+  return ranking;
+};
+
+// sort descending by val property
+var sdesc = function(a, b) {return b.val - a.val};
+
+// get values divided by relation
+var getRelation = function(unrelated, relation, sortorder) {
+  var related = [];
+  $.each(unrelated, function(idx, item){
+    if (countrystats[year][item.label].hasOwnProperty(relation)) {
+      related.push({
+        val: item.val / countrystats[year][item.label][relation],
+        label: item.label
+      })
+    }
+  });
+  if (sortorder == 'desc')
+    related = related.sort(sdesc);
+  return related;
+};
+
+var setAidRelations = function(source, target) {
+  reldonors = source;
+  relrecipients = target;
+  maxdonated = reldonors[0].val;
+  maxreceived = relrecipients[0].val;
+};
+
+var setRelations = function(source, target) {
+  if ('norelate' == relation) {
+    setAidRelations(source.slice(), target.slice());
+  } else {
+    setAidRelations(getRelation(donors, relation, 'desc'), getRelation(recipients, relation, 'desc'));
+  }
+  if (!scatterrelation) {
+    scatterrelation = iselect.find('option:first')[0].value;
+  }
+};
+
+var donorBars = function(text) {
+  var r = doLimit ? reldonors.slice(0, getLimit()) : reldonors.slice();
+  bar('#aiddonors', aidRanking(r, 'Aid donated in USD: ' + text + ': '));
+  bar('#donorstransparency', indicatorRanking(r, 'aidtransparency'));
+}
+
+var recipientBars = function(text) {
+  var r = doLimit ? relrecipients.slice(0, getLimit()) : relrecipients.slice();
+  bar('#aidrecipients', aidRanking(r, 'Aid received in USD: ' + text + ': '));
+  bar('#recipientstransparency', indicatorRanking(r, 'IQ.CPA.TRAN.XQ'));
+}
+
+var showGraphs = function(text) {
+  donorBars(text);
+  recipientBars(text);
+  scatterplot('#aidrelations', spdata(relrecipients, scatterrelation));
+};
+
+var getLimit = function(){
+  if (doLimit) return defaultlimit;
+  return 0;
+};
+
+/********** main program flow **********/
+
+(function() {
+
+// lists of donors and recipients sorted by aid sums descending
+donors = ranks[year]['donated'].reverse();
+recipients = ranks[year]['received'].reverse();
+
+// copy values not reference to array
+reldonors = donors.slice();
+relrecipients = recipients.slice();
+
+// these values are overwritten when calculating relations and are reset when
+// showing totals
+maxdonated = reldonors[0].val;
+maxreceived = relrecipients[0].val;
+
+// load geo data and draw map
+d3.json('/json/world-countries.json', function(error, json) {
+  geocountries = json.features;
+  drawmap(geocountries, getQuantize(), showLinks);
+  drawlegend();
+});
+
+// fill indicators select lists
+iselect = $('#indicators');
+$.each(indicators, function(i) {
+  if ('global' == indicators[i].type)
+    iselect.append('<option value="' + indicators[i].id + '">' + indicators[i].label + '</option')
+});
+// additional indicators
+iselect.append('<option value="userrequests">Google User Data Requests</option');
+
+// set relations and show graphs
+setRelations(donors, recipients);
+showGraphs($('.active .relate').text());
+
+/***** events *****/
+
+// indicator selection
+iselect.change(function(e) {
+  e.preventDefault();
+  scatterrelation = $(this).val();
+  scatterplot('#aidrelations', spdata(relrecipients, scatterrelation));
+});
+
+// calculate relations and redraw graphs
+$('.relate').click(function(e){
+  e.preventDefault();
+  $('.relate').parent('li').removeClass('active');
+  $(this).parent('li').attr('class', 'active');
+  relation = this.id;
+  setRelations(donors, recipients);
+  showGraphs(this.innerHTML);
+  drawmap(geocountries, getQuantize(), showLinks);
+});
+
+// expand / reduce bar charts
+$('.menulimit').click(function(e) {
+  e.preventDefault();
+  var button = $(this);
+  var bid = button.attr('id');
+  var text = $('li.active a.relate').text();
+  if ('expand' == bid) {
+    $('.expand').hide();
+    $('.reduce').show();
+    doLimit = false;
+  } else {
+    $('.reduce').hide();
+    $('.expand').show();
+    doLimit = true;
+  }
+  showGraphs(text);
+});
+
+})();
