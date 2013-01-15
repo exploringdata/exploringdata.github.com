9179e4edea3fd26b07eae5b25a976759f3420b20
Ramiro Gomez <web@ramiro.org>
2013-01-15 19:38:17 +0100

    new deployment

---
+++ js/customsearch.js
@@ -0,0 +1,7 @@
+(function() {
+  var cx = '017337174147211687926:ocjb1eiqcfw';
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
