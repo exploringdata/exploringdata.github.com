c8ac430c9d0d7a7f245f73713d8691ee01753d65
Ramiro Gómez <code@ramiro.org>
2014-06-06 19:52:44 +0200

    new deployment

--- js/human-disease-network/script.js
+++ js/human-disease-network/script.js
@@ -11,8 +11,8 @@ $(function(){
     graph: {
       minNodeSize: .5,
       maxNodeSize: 15,
-      minEdgeSize: 1,
-      maxEdgeSize: 1
+      minEdgeSize: .3,
+      maxEdgeSize: 1.5
     },
     forceLabel: 0,
     type: 'directed'
