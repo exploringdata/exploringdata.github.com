a3d62c48537dfca3d350307dae42dc8e52e70f49
Ramiro Gómez <code@ramiro.org>
2017-12-14 21:38:26 +0100

    new deployment

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
 // adapt containerwidth to screen size
 var containerwidth = function(selector) {
-  return parseInt($(selector).width())
+    return parseInt($(selector).width())
 };
 function containerDim(selector, dim) {
     return parseInt(d3.select(selector).style(dim))
 }
 $(function(){
-  $('.nav a[href="'+document.location.pathname+'"]').parent('li').attr('class', 'active');
-  $('.share a').click(function(e){
-    e.preventDefault();
-    var b = $(this);
-    var action = $.trim(b.find('i').attr('class').replace('icon-', ''));
-    _gaq.push(['_trackEvent', 'Sharemenu', action]);
-    window.open(b.attr('href'));
-  });
-  $('.showhelp').click(function(e){
-    e.preventDefault();
-    $('#help').modal();
-  });
+    $('.nav a[href="'+document.location.pathname+'"]').parent('li').attr('class', 'active');
+    $('.showhelp').click(function(e){
+        e.preventDefault();
+        $('#help').modal();
+    });
 });
