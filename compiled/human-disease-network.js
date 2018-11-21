3737d93ccb65800dd4b4b5264835e0c63a824bea
Ramiro Gómez <code@ramiro.org>
2018-11-21 14:48:02 +0100

    new deployment

---
+++ compiled/human-disease-network.js
@@ -0,0 +1,1 @@
+$(function(){var props={drawing:{defaultLabelColor:'#fff',defaultLabelSize:12,defaultLabelBGColor:'#fff',defaultLabelHoverColor:'#000',labelThreshold:4,defaultEdgeType:'curve'},graph:{minNodeSize:.5,maxNodeSize:15,minEdgeSize:.2,maxEdgeSize:1},forceLabel:0,type:'directed'};visgexf.init('sig','/gexf/human-disease-network.json',props)});
