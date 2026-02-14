4c17e1159fede7219018b96b5193bfc8361e0c9e
Ramiro Gómez <code@ramiro.org>
2026-02-14 03:15:20 +0100

    new deployment

--- js/customsearch.js
+++ js/customsearch.js
@@ -1,16 +1,19 @@
 (function() {
-  var cx = '017337174147211687926:ocjb1eiqcfw';
-  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-      '//www.google.com/cse/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+  const cx = '017337174147211687926:ocjb1eiqcfw';
+  const gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = `${window.location.protocol}//www.google.com/cse/cse.js?cx=${cx}`;
+
+  const s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
 })();
 
-jQuery(function($){
-  //$('#gcse-data').submit(function)
-  var query = decodeURIComponent(document.location.search.replace(/\?.*q=/, '')).replace(/\+/g, ' ');
+document.addEventListener('DOMContentLoaded', function() {
+  const query = decodeURIComponent(window.location.search.replace(/\?.*q=/, '')).replace(/\+/g, ' ');
+
   if (query) {
-    $('#gsc-search').removeClass('hidden');
-    $('#q').val(query);
+      document.getElementById('gsc-search').classList.remove('hidden');
+      document.getElementById('q').value = query;
   }
 });
